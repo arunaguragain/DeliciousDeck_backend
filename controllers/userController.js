0c2b6dbe121f984d89fe71e3109e6439fdef7df7
Unika1 <guragainaruna@gmail.com>
2025-02-19 15:15:35 +0545

    modified user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,135 +1,3 @@
-// const User = require('../model/User')
-// const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcrypt');
-
-// const registerUser = async(req, res)=>{
-//     const {username, password} = req.body;
-//     //validate username and password
-//     if(!username || !password){
-//         return res.status(400).json({
-//             error: "Please Insert username and password"
-//         })
-//     }
-//     try{
-//         const checkExistingUser = await User.findOne({where: {username}})
-//         if(checkExistingUser){
-        
-//             return res.status(400).json({
-//                 error: "New user required"
-//             })
-           
-//         }
-//         const saltRound = 10;
-//         const hashpassword = await bcrypt.hash(password, saltRound)
-
-//         const newUser = await User.create({username, password: hashpassword});
-//         res.status(200).json({message: "Registartion Successful....."});
-
-//     }
-//     catch(error){
-//         console.log(error)
-//         res.status(500).json({error: "Something went Wrong"});
-//     }
-
-// }
-
-// const loginUser = async(req, res) =>{
-//     const {username, password} = req.body;
-//      //validate username and password
-//     if(!username || !password){
-//         return res.status(400).json({
-//             error: "Please Insert username and password"
-//         })
-//     }
-//     try{
-//         const user = await User.findOne({where: {username}})
-//         if(!user){
-//             return res.status(400).json({
-//                 error: "New user required"
-//             })
-//         }
-//         const isMatch = await bcrypt.compare(password, user.password)
-//         if(!isMatch){
-//             return res.status(400).json({
-//                 error: "Insert proper password!!!!"
-//             })
-//         }
-//         const token = jwt.sign(
-//             {id: user.username, username: user.username},
-//             process.env.JWT_SECRET || 'FVHJAFJHSFVBSFBSSFJSF',
-//             {expiresIn: '24h'}
-
-//         )
-//         res.status(200).json({message: "Successfully Logged in", token},
-            
-
-//         )
-//     }
-//     catch(error){
-//         res.status(500).json({error: "Something went Wrong"});
-//         console.log(error)
-    
-//     }
-
-// }
-// // const getUser = async(req, res)=>{
-
-// //     try{
-// //         const tests = await User.findAll();
-// //         res.status(200).json(tests);
-
-// //     }
-// //     catch(error){
-// //         res.status(500).json({error: "Failed to Load"})
-// //     }
-// // }
-
-// // const createUser = async(req, res)=>{
-    
-// //     try{
-        
-// // const {username, password} = req.body;
-
-// // //Hash the password
-// // const newtest = await User.create({username, password})
-
-// // res.status(200).json(newtest);
-// //     }
-// //     catch(error){
-// //         res.status(500).json({error: "Failed to Load"})
-// //         console.log(error)
-// //     }
-
-// // }
-
-// // const updateUser = async(req, res)=>{
-// //     try {
-// //         const user = await User.findByPk(req.params.id);
-// //         if (!user) {
-// //             return res.status(404).json({ message: 'User not found' });
-// //         }
-// //         await user.update(req.body);
-// //         res.json(user);
-// //     } catch (err) {
-// //         res.status(400).json({ error: err.message });
-// //     }
-// // }
-
-// // const deleteUser = async(req, res)=>{
-// //     try {
-// //         const user = await User.findByPk(req.params.id);
-// //         if (!user) {
-// //             return res.status(404).json({ message: 'User not found' });
-// //         }
-// //         await user.destroy();
-// //         res.json({ message: 'User deleted' });
-// //     } catch (err) {
-// //         res.status(500).json({ error: err.message });
-// //     }
-// // }
-
-// // module.exports = {createUser, getUser, deleteUser, updateUser}
-// module.exports = {registerUser, loginUser}
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const  User  = require('../model/User');
@@ -148,13 +16,15 @@ const  User  = require('../model/User');
 // Register a new user
 const registerUser = async (req, res) => {
     try {
-        const { fullName, dob, email, address, password } = req.body; // Updated to match model fields
+        const { fullName, dob, email, address, password, role } = req.body; // Updated to match model fields
 
         // Check if user already exists
         const userExists = await User.findOne({ where: { email } }); // Check by email instead of username
         if (userExists) {
             return res.status(400).json({ message: 'User already exists' });
         }
+
+        const userRole = role || 'user';
 
         // Hash the password
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -199,7 +69,7 @@ const loginUser = async (req, res) => {
             { expiresIn: '1h' }
         );
 
-        res.status(200).json({ message: 'Login successful', token, userId: user.userId });
+        res.status(200).json({ message: 'Login successful', token, userId: user.userId,role: user.role });
     } catch (err) {
         res.status(500).json({ message: 'Error logging in', error: err.message });
     }
