fb73e2e543583bc018ad796f07032a6a18b81693
Unika1 <guragainaruna@gmail.com>
2025-02-26 13:53:37 +0545

    Updated reservationRoute

--- routes/reservationRoute.js
+++ routes/reservationRoute.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const authMiddleware  = require('../middleware/authorization');
 const reservationController = require('../controllers/reservationController');
 
-// Check if controller functions are properly imported
 if (!reservationController.createReservation) {
     console.error("createReservation function is undefined!");
 }
@@ -16,19 +15,14 @@ const {
     deleteReservation 
 } = reservationController;
 
-// Create a reservation route (POST)
 router.post('/create', authMiddleware(), createReservation);
 
-// Get all reservations route (GET)
 router.get('/', authMiddleware(), getReservations);
 
-// Get a specific reservation by ID route (GET)
 router.get('/:id', authMiddleware(), getReservation);
 
-// Update a reservation route (PUT)
 router.put('/:id', authMiddleware(), updateReservation);
 
-// Delete a reservation route (DELETE)
 router.delete('/:id', authMiddleware(), deleteReservation);
 
 module.exports = router;
