616e81223105c2e2f30b13c5ef5532a0ab8259a8
Unika1 <guragainaruna@gmail.com>
2025-02-26 13:58:50 +0545

    Updated image uplaod

--- middleware/imageUpload.js
+++ middleware/imageUpload.js
@@ -14,9 +14,9 @@ const storage = multer.diskStorage({
 // File filter to allow only images
 const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image/')) {
-    cb(null, true); // Accept file
+    cb(null, true); 
   } else {
-    cb(new Error('Only image files are allowed!'), false); // Reject file
+    cb(new Error('Only image files are allowed!'), false); 
   }
 };
 
