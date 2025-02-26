b0da91b664e02fe3f9e0ee23e924ddefde95f082
Unika1 <guragainaruna@gmail.com>
2025-02-26 13:52:27 +0545

    Final commit of menuItemRoute

--- routes/menuItemRoute.js
+++ routes/menuItemRoute.js
@@ -5,14 +5,10 @@ const { createMenuItem, getMenuItems, getMenuItem, deleteMenuItem  } = require('
 
 router.get('/', getMenuItems);
 
-// Public route: Get a specific menu item
 router.get('/:id', getMenuItem);
 
-// Protected route: Create a new menu item (Requires Authentication)
 router.post('/create', authMiddleware(), createMenuItem);
 
-
-// Protected route: Delete a menu item (Requires Authentication)
 router.delete('/:id', authMiddleware(), deleteMenuItem);
 
 module.exports = router;
